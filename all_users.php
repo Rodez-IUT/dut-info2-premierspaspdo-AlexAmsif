a204015c5ace28c8a40da3292566adb2da45473a
AlexAmsif <alex.amsif@gmail.com>
2019-11-06 10:27:50 +0100

    En cours

---
+++ all_users.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Les excuses du lundi matin</title>
+	  
+		<link href="css/monStyle.css" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+	</head>
+<body>
+<?php
+$host ='localhost';
+$db='my-activities';
+$user='root';
+$pass='root';
+$charset='utf8mb4';
+
+$dsn="mysql:host=$host;dbname=$$db;charset=$charset";
+$options = {
+	PDO::ATTR_ERRMODE			=> PDO::ERRMODE_EXCEPTION,
+	PDO::ATT_DEFAUT_FETCH_MODE  => PDO::FETCH_ASSOC,
+	PDO:ATTR_EMULATE_PREPARES   => false,
+};
+try{
+	$pdo = new PDO($dsn,$user,$pass,$options);
+}catch{
+	throw new PDOException($e->getMessage(),(int)$e->getCode());
+}
+$stmt = $pdo->query('SELECT * FROM users ORDER BY username ASC');
+var_dump($stmt);
+?>
+
+</body>
+</html>
